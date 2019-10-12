7c0c0cfd3cd848b0bb304f19e2c45b0890ead48c
Jacob Ward <w.jacob.ward@gmail.com>
2019-10-12 10:30:45 -0400

    Refactored to get rid of separate objects for each color.

--- PenteBoard.py
+++ PenteBoard.py
@@ -5,9 +5,8 @@ class PenteBoard:
     stone_color = enum.Enum('stone', 'black white')
 
     def __init__(self):
-
-        self.black_stones = [[0 for x in range(19)] for x in range(19)]
-        self.white_stones = [[0 for x in range(19)] for x in range(19)]
+        board_grid = [[[0 for x in range(19)] for x in range(19)] for x in range(2)]
+        self.current_board = dict(zip(self.stone_color, board_grid))
 
     def place_stone(self, color, location):
         self.add_stone(color, location)
@@ -15,39 +14,20 @@ class PenteBoard:
         self.check_for_win(color)
 
     def add_stone(self, color, location):
-        selector = {self.stone_color.black: self.add_black_stone,
-                    self.stone_color.white: self.add_white_stone}
-        selector[color](location)
+        valid_spaces = self.get_valid_moves()
+        if valid_spaces[location[0]][location[1]] == 0:
+            raise SpaceOccupiedError("Space already occupied")
+        self.current_board[color][location[0]][location[1]] = 1
 
     def remove_stone(self, color, location):
-        selector = {self.stone_color.black: self.remove_black_stone,
-                    self.stone_color.white: self.remove_white_stone}
-        selector[color](location)
+        if not self.current_board[color][location[0]][location[1]] == 1:
+            raise SpaceEmptyError("Attempted to remove a stone that wasn't there")
+        self.current_board[color][location[0]][location[1]] = 0
 
     def get_valid_moves(self):
-        occupied_spaces = [[sum(x) for x in zip(self.black_stones[row], self.white_stones[row])] for row in range(len(self.black_stones))]
+        occupied_spaces = [[sum(x) for x in zip(self.current_board[self.stone_color.black][row], self.current_board[self.stone_color.white][row])] for row in range(len(self.current_board[self.stone_color.black]))]
         valid_spaces = [[1-x for x in row] for row in occupied_spaces]
         return valid_spaces
-
-    def add_black_stone(self, location):
-        if self.white_stones[location[0]][location[1]] == 1:
-            raise SpaceOccupiedError("Space already occupied by white")
-        self.black_stones[location[0]][location[1]] = 1
-
-    def add_white_stone(self, location):
-        if self.black_stones[location[0]][location[1]] == 1:
-            raise SpaceOccupiedError("Space already occupied by black")
-        self.white_stones[location[0]][location[1]] = 1
-
-    def remove_black_stone(self, location):
-        if not self.black_stones[location[0]][location[1]] == 1:
-            raise SpaceEmptyError("Attempted to remove a black stone that wasn't there")
-        self.black_stones[location[0]][location[1]] = 0
-
-    def remove_white_stone(self, location):
-        if not self.white_stones[location[0]][location[1]] == 1:
-            raise SpaceEmptyError("Attempted to remove a white stone that wasn't there")
-        self.white_stones[location[0]][location[1]] = 0
 
     def check_for_capture(self, color, location):
         # look for two opposing stones sandwiched by two stones of <color>, one being the newly placed stone
@@ -55,13 +35,11 @@ class PenteBoard:
 
     def check_for_win(self, color):
         # look for lines of 5 or more stones of the same color
-        selector = {self.stone_color.black: self.black_stones,
-                    self.stone_color.white: self.white_stones}
-        stones_to_check = selector[color]
         # check for horizontal win
         # check for vertical win
         # check for diagonal win (SW/NE)
         # check for diagonal win (NW/SE)
+        pass
 
 
 class SpaceOccupiedError(Exception):
